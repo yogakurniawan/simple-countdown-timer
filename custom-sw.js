9a9dd62856fddf6c8b7407719c17792d258b9f4c
Yoga Kurniawan <yoga.kwn@gmail.com>
2018-07-13 13:42:19 +0700

    Updates

--- custom-sw.js
+++ custom-sw.js
@@ -1,3 +1,3 @@
-importScripts("/simple-countdown-timer/precache-manifest.f3227605c2fc9bd1e61bc4013aa3839d.js", "https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+importScripts("/simple-countdown-timer/precache-manifest.e15960da010e43cf890c5840b94068bd.js", "https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
 
 console.log('Hello from sw.js');
